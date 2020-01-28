7dfb239d6cb167f93c47484259d8338041a745e5
mineshmane <mineshmane94@gmail.com>
2020-01-28 10:44:52 +0530

    Added:UC1 fisrtName with pattern validtion

--- shell.sh
+++ shell.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x 
 echo welcome to User registration
+
+read -p "Enter user first Name: " firstName
+
+firstNamePattern="^[A-Z]+[a-zA-Z]{2,}"
+
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "valid $firstName"
+else
+	echo "invalid $firstName"
+fi
