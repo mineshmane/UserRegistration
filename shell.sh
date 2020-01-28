83c900ceb01ba931ee35a239166b9b6b54f3eae4
mineshmane <mineshmane94@gmail.com>
2020-01-28 11:11:01 +0530

    Added:UC2 Lastname validation done

--- shell.sh
+++ shell.sh
@@ -2,12 +2,28 @@
 echo welcome to User registration
 
 read -p "Enter user first Name: " firstName
+read -p "Enter user last Name: " lastName
+namePattern="^[A-Z]+[a-zA-Z]{2,}$"
 
-firstNamePattern="^[A-Z]+[a-zA-Z]{2,}"
+function firstNameValidation(){
 
-if [[ $firstName =~ $firstNamePattern ]]
-then
-	echo "valid $firstName"
-else
-	echo "invalid $firstName"
-fi
+	if [[ $firstName =~ $namePattern ]]
+	then
+		echo "Valid $firstName"
+	else
+		echo "Invalid $firstName"
+	fi
+}
+
+function lastNameValidation(){
+
+	if [[ $lastName =~ $namePattern ]]
+	then
+		echo "Valid $lastName"
+	else
+		echo "Invalid $lastName"
+	fi
+}
+
+firstNameValidation
+lastNameValidation
